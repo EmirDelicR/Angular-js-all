d692c9841b01482b1ee8469b7c6b4bded77cce61
Emir <emirdelictbf@gmail.com>
2020-03-31 18:23:37 +0200

    Update password equal validation

--- 0-projects/practicing/directives/src/app/shared/password-validator.directive.ts
+++ 0-projects/practicing/directives/src/app/shared/password-validator.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, forwardRef, Attribute } from '@angular/core';
+import { Directive, Attribute } from '@angular/core';
 import { Validator, AbstractControl, NG_VALIDATORS } from '@angular/forms';
 
 @Directive({
@@ -6,63 +6,23 @@ import { Validator, AbstractControl, NG_VALIDATORS } from '@angular/forms';
   providers: [
     {
       provide: NG_VALIDATORS,
-      useExisting: forwardRef(() => PasswordValidator),
+      useExisting: PasswordValidator,
       multi: true
     }
   ]
 })
 export class PasswordValidator implements Validator {
   constructor(
-    @Attribute('appPasswordValidator') public validateEqual: string,
-    @Attribute('reverse') public reverse: boolean
+    @Attribute('appPasswordValidator') public validateEqual: string
   ) {}
 
-  private get isReverse() {
-    if (!this.reverse) {
-      return false;
-    }
-    return this.reverse ? true : false;
-  }
+  validate(control: AbstractControl): { [key: string]: boolean } | null {
+    const compareTo = control.parent.get(this.validateEqual);
 
-  validate(confirmPasswordInput: AbstractControl): { [key: string]: any } {
-    const confirmValue = confirmPasswordInput.value;
-
-    const passwordInput = confirmPasswordInput.root.get(this.validateEqual);
-
-    if (
-      passwordInput &&
-      confirmValue !== passwordInput.value &&
-      !this.isReverse
-    ) {
-      return {
-        validateEqual: false
-      };
+    if (compareTo && compareTo.value !== control.value) {
+      return { notEqual: true };
     }
 
-    if (passwordInput && confirmValue !== passwordInput.value) {
-      return {
-        validateEqual: false
-      };
-    }
-
-    if (
-      passwordInput &&
-      confirmValue === passwordInput.value &&
-      this.isReverse
-    ) {
-      delete passwordInput.errors.appPasswordValidator;
-      if (!Object.keys(passwordInput.errors).length) {
-        passwordInput.setErrors(null);
-      }
-    }
-
-    if (
-      passwordInput &&
-      confirmValue !== passwordInput.value &&
-      this.isReverse
-    ) {
-      passwordInput.setErrors({ appPasswordValidator: false });
-    }
     return null;
   }
 }
